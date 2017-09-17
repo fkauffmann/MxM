c35771f9be4db9be8204db549b860ebcba4c02cd
Fabrice Kauffmann <fabrice.kauffmann@gmail.com>
2017-09-17 14:08:31 +0200

    Contacts controler added

--- routes/contacts.js
+++ routes/contacts.js
@@ -58,4 +58,138 @@ router.get('/', function(req, res, next) {
     });
 });
 
+// Add Contact (create)
+router.get('/add', (req, res, next) => {
+  res.render('contactedit', {
+    title: "Add a Contact",
+    docreate: true,
+    contactid: "",
+    contact: undefined,
+		contacttypelist: config.contactTypes        
+  });
+});
+
+// Save Category (update)
+router.post('/save', (req, res, next) => {
+  var p;
+  if (req.body.docreate === "create") {
+    p = contacts.create(guid(),
+      req.body.firstname, req.body.lastname, req.body.contacttype, req.body.enterprise, req.body.job, req.body.meetat, req.body.meetdate, req.body.street, req.body.postcode, req.body.city, req.body.country, req.body.mail, req.body.website, req.body.phone, req.body.cancontact ? true : false, req.body.note);
+  } else {
+    p = categories.update(req.body.contactid,
+      req.body.firstname, req.body.lastname, req.body.contacttype, req.body.enterprise, req.body.job, req.body.meetat, req.body.meetdate, req.body.street, req.body.postcode, req.body.city, req.body.country, req.body.mail, req.body.website, req.body.phone, req.body.cancontact ? true : false, req.body.note);
+  }
+  p.then(contact => {
+      //res.redirect('/categories/view?categoryid=' + req.body.categoryid);
+      res.redirect('/contacts');
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// Read Category (read)
+router.get('/view', (req, res, next) => {
+  contacts.read(req.query.contactid)
+    .then(contact => {
+      var fullname = "";
+
+      if (contact.lastname != null && contact.firstname != null) {
+        fullname = contact.lastname.toUpperCase() + ' ' + contact.firstname;
+      }
+      if (contact.lastname != null && contact.firstname == null) {
+        fullname = contact.lastname.toUpperCase();
+      }
+      if (contact.lastname == null && contact.firstname != null) {
+        fullname = contact.firstname;
+      }
+      if (contact.lastname == null && contact.firstname == null) {
+        fullname = contact.enterprise;
+      }
+
+      res.render('contactview', {
+        title: contact ? ("View Contact {" + fullname + "}") : "",
+        contactid: req.query.contactid,
+        contact: contact,
+		    contacttypelist: config.contactTypes
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// Edit Contact (update)
+router.get('/edit', (req, res, next) => {
+  contacts.read(req.query.contactid)
+    .then(contact => {
+      var fullname = "";
+
+      if (contact.lastname != null && contact.firstname != null) {
+        fullname = contact.lastname.toUpperCase() + ' ' + contact.firstname;
+      }
+      if (contact.lastname != null && contact.firstname == null) {
+        fullname = contact.lastname.toUpperCase();
+      }
+      if (contact.lastname == null && contact.firstname != null) {
+        fullname = contact.firstname;
+      }
+      if (contact.lastname == null && contact.firstname == null) {
+        fullname = contact.enterprise;
+      }
+
+      res.render('contactedit', {
+        title: contact ? ("Edit Contact {" + fullname + "}") : "Add a Contact",
+        docreate: false,
+        contactid: req.query.contactid,
+        contact: contact,
+		    contacttypelist: config.contactTypes
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// Ask to delete Contact (delete)
+router.get('/delete', (req, res, next) => {
+  contacts.read(req.query.contactid)
+    .then(contact => {
+      var fullname = "";
+
+      if (contact.lastname != null && contact.firstname != null) {
+        fullname = contact.lastname.toUpperCase() + ' ' + contact.firstname;
+      }
+      if (contact.lastname != null && contact.firstname == null) {
+        fullname = contact.lastname.toUpperCase();
+      }
+      if (contact.lastname == null && contact.firstname != null) {
+        fullname = contact.firstname;
+      }
+      if (contact.lastname == null && contact.firstname == null) {
+        fullname = contact.enterprise;
+      }
+
+      res.render('contactdelete', {
+        title: contact ? ("Delete Contact {" + fullname + "}") : "",
+        contactid: req.query.contactid,
+        contact: contact
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// Really delete Contact (delete)
+router.post('/delete/confirm', (req, res, next) => {
+  contacts.delete(req.body.contactid)
+    .then(() => {
+      res.redirect('/contacts');
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 module.exports = router;
