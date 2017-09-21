d41f6a7ec5ad02a6c503a61895ebe662608e17d3
Fabrice Kauffmann <fabrice.kauffmann@gmail.com>
2017-09-21 12:42:02 +0200

    Default port changed to 8000

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.version = "1.0";
-config.port = "80";
+config.port = "8000";
 config.units =  ['Piece','Hour'];
 config.contactTypes =  ['Supplier','Customer','Event Manager','Other'];
 
